68dcac33603afc7f92cc71c9afcc9153da6dd546
tedmartins <tedmartins.me@gmail.com>
2021-04-22 18:39:14 -0300

    aula 4

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,7 +1,40 @@
+import Image from 'next/image';
+import { useContext, useRef, useEffect } from 'react';
+import Slider from 'rc-slider';
+
+import 'rc-slider/assets/index.css';
+
+import { PlayerContext } from '../../contexts/PlayerContext';
 
 import styles from './styles.module.scss';
 
 export function Player() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const { 
+    episodeList, 
+    currentEpisodeIndex, 
+    isPlaying, 
+    togglePlay,
+    setPlayingState 
+  } = useContext(PlayerContext)
+
+  useEffect(() => {
+    if (!audioRef.current) {
+      return;
+    }
+
+    if (isPlaying) {
+      audioRef.current.play()
+    } else {
+      {
+        audioRef.current.pause()
+      }
+    }
+
+  }, [isPlaying])
+
+  const episode = episodeList[currentEpisodeIndex]
 
   return (
     <div className={styles.playerContainer}>
@@ -10,34 +43,74 @@ export function Player() {
         <strong>Tocando Agora</strong>
       </header>
 
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um podcast para ouvir</strong>
-      </div>
+      { episode ? ( 
+        <div className={styles.currentEpisode}>
+          <Image 
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
 
-      <footer className={styles.empty}>
+          <strong>{episode.title}</strong>
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+      <div className={styles.emptyPlayer}>
+      <strong>Selecione um podcast para ouvir</strong>
+      </div>
+      ) }
+
+      <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            { episode ? (
+              <Slider 
+                trackStyle={{ backgroundColor: '#04D361' }}
+                railStyle={{ backgroundColor: '#9F75FF' }}
+                handleStyle={{ borderColor: '#04D361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            ) }
           </div>
           
           <span>00:00</span>
         </div>
 
+        { episode && (
+          <audio 
+            src={episode.url}
+            ref={audioRef}
+            autoPlay
+            onPlay={() => setPlayingState(true)}
+            onPause={() => setPlayingState(false)}
+          />
+        )}
+
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/img/shuffle.svg" alt="Embaralhar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/img/play-previous.svg" alt="Tocar anterior" />
           </button>
-          <button type="button" className={styles.playButton}>
-            <img src="/img/play.svg" alt="Tocar" />
+          <button 
+            type="button" 
+            className={styles.playButton} 
+            disabled={!episode} 
+            onClick={togglePlay}
+          >
+            { isPlaying 
+              ? <img src="/img/pause.svg" alt="Pausado" />
+              : <img src="/img/play.svg" alt="Tocar" />
+            }
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/img/play-next.svg" alt="Tocar próxima" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/img/repeat.svg" alt="Repetir" />
           </button>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
+import { useContext } from 'react';
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR'
+
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationtoTimeString';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from './home.module.scss';
+
 
 type Episode = {
   id: string;
   title: string;
   thumbnail: string;
-  description: string;
   members: string;
   duration: number;
   durationAsString: string;
@@ -25,6 +29,7 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext)
 
   return (
     <div className={styles.homepage}>
@@ -44,13 +49,15 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                 />
 
                 <div className={styles.episodeDetails}>
-                  <a href="">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
                   <p>{episode.members}</p>
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/img/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
@@ -60,7 +67,51 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
       </section>
 
       <section className={styles.allEpisodes}>
+          <h2>Todos Episódios</h2>
 
+          <table cellSpacing={0}>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Podcast</th>
+                <th>Integrantes</th>
+                <th>Data</th>
+                <th>Duração</th>
+                <th></th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {allEpisodes.map(episode => {
+                return (
+                  <tr key={episode.id}>
+                    <td style={{ width: 72 }}>
+                      <Image 
+                        width={120}
+                        height={120}
+                        src={episode.thumbnail}
+                        alt={episode.title}
+                        objectFit="cover"
+                      />
+                    </td>
+                    <td>
+                      <Link href={`/episodes/${episode.id}`}>
+                        <a>{episode.title}</a>
+                      </Link>
+                    </td>
+                    <td>{episode.members}</td>
+                    <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                    <td>{episode.durationAsString}</td>
+                    <td>
+                      <button type="button">
+                        <img src="/img/play-green.svg" alt="Tocar episódio"/>
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
       </section>
     </div>
   );
@@ -84,7 +135,6 @@ export const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url,
     };
   })
